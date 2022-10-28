b565b739f5a2b9733c0ebfbcb1e4db775b77d4bf
Sai Rithvik Vaikuntam <svaikunt@asu.edu>
2022-10-27 23:33:07 -0700

    Test cases of readFromFile Method

--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -40,36 +40,69 @@ public class urinalsTest {
     @Test
     void Test1countFreeUrinals() {
         System.out.println("====== Sai Rithvik Vaikuntam == TEST ONE EXECUTED =======");
-        assertEquals(1, urine.countFreeUrinals("10001"));
+        assertEquals(1, m.countFreeUrinals("10001"));
     }
 
     @Test
     void Test2countFreeUrinals() {
         System.out.println("====== Sai Rithvik Vaikuntam == TEST TWO EXECUTED =======");
-        assertEquals(0, urine.countFreeUrinals("1001"));
+        assertEquals(0, m.countFreeUrinals("1001"));
     }
 
     @Test
     void Test3countFreeUrinals() {
         System.out.println("====== Sai Rithvik Vaikuntam == TEST THREE EXECUTED =======");
-        assertEquals(3, urine.countFreeUrinals("00000"));
+        assertEquals(3, m.countFreeUrinals("00000"));
     }
 
     @Test
     void Test4countFreeUrinals() {
         System.out.println("====== Sai Rithvik Vaikuntam == TEST FOUR EXECUTED =======");
-        assertEquals(2, urine.countFreeUrinals("0000"));
+        assertEquals(2, m.countFreeUrinals("0000"));
     }
 
     @Test
     void Test5countFreeUrinals() {
         System.out.println("====== Sai Rithvik Vaikuntam == TEST FIVE EXECUTED =======");
-        assertEquals(1, urine.countFreeUrinals("01000"));
+        assertEquals(1, m.countFreeUrinals("01000"));
     }
 
     @Test
     void Test6countFreeUrinals() {
         System.out.println("====== Sai Rithvik Vaikuntam == TEST SIX EXECUTED =======");
-        assertEquals(-1, urine.countFreeUrinals("011"));
+        assertEquals(-1, m.countFreeUrinals("011"));
+    }
+
+    @Test
+    void Test1readFromFile() throws IOException {
+        System.out.println("====== Sai Rithvik Vaikuntam == readFromFileOne == TEST ONE EXECUTED - Valid Case =======");
+        String filePath = "src/urinal.dat";
+        assertArrayEquals(new String[]{"10001", "1001", "00000", "0000", "01000", "011"}, m.readFromFile(filePath));
+    }
+
+    @Test
+    void Test2FromFile() {
+        System.out.println("====== Sai Rithvik Vaikuntam == readFromFileTwo == TEST TWO EXECUTED - File doesn't exist =======");
+        String filePath = "urinal.dat1";
+        assertThrows(FileNotFoundException.class, () -> {
+            m.readFromFile(filePath);
+        });
+    }
+
+    @Test
+    void Test3FromFile() throws IOException {
+        System.out.println("====== Sai Rithvik Vaikuntam == readFromFileThree == TEST THREE EXECUTED - Empty file =======");
+        String filePath = "src/urinal.dat";
+        String [] inputs = m.readFromFile(filePath);
+        assertArrayEquals(inputs, m.readFromFile(filePath));
+    }
+
+    @Test
+    void Test4readFromFile() {
+        System.out.println("====== Sai Rithvik Vaikuntam == readFromFileFour == TEST FOUR EXECUTED - IOException =======");
+        String filePath = "urinal";
+        assertThrows(IOException.class, () -> {
+            m.readFromFile(filePath);
+        });
     }
 }
