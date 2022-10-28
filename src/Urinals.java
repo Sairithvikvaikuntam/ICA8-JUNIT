117802802873e5c5439f435d7c6f288ed4311afb
Sai Rithvik Vaikuntam <svaikunt@asu.edu>
2022-10-27 23:55:26 -0700

    Main class to execute the code

--- src/Urinals.java
+++ src/Urinals.java
@@ -10,21 +10,34 @@ import java.util.Scanner;
 
 
 public class Urinals {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int input_type;
-        System.out.println("Select Input Type:\n" + "1 -> From Console\n" + "2 -> Use urinal.dat file");
-        input_type = sc.nextInt();
-        String str = null;
-        if (input_type == 1) {
-            System.out.println("Enter the String in the console");
-            str = sc.next();
-        } else if (input_type == 2) {
-
+    public static void main(String[] args) throws IOException {
+        Methods urinals = new Methods();
+        while (true) {
+            System.out.println("\n===========");
+            System.out.println("1) Read from console \n2) Read from file\n3) Exit");
+            System.out.print("Enter your option: ");
+            Scanner sc = new Scanner(System.in);
+            String next = sc.nextLine();
+            switch (next) {
+                case "1" -> {
+                    System.out.print("Provide input string: ");
+                    String inputString = sc.nextLine();
+                    if (!urinals.goodString(inputString)) {
+                        System.out.println("Invalid input");
+                        continue;
+                    }
+                    int count = urinals.getFreeUrinals(inputString);
+                    System.out.print("Free urinals : " + count + "\n");
+                }
+                case "2" -> {
+                    String[] lines = urinals.readFromFile("src/urinal.dat");
+                    urinals.writeToFile(lines);
+                }
+                case "3" -> {
+                    System.out.println("Exit...");
+                    System.exit(1);
+                }
+            }
         }
-        if (Methods.goodString(str)) {
-            System.out.println("Counts Urinals");
-        }
-        else
     }
 }
